00fae6dc4ea9dad1ca983c5f1f3653a12dd4b835
OrZipori <or_zipori@walla.co.il>
2017-06-17 19:08:42 +0300

    thread pool

--- ex42.c
+++ ex42.c
@@ -23,6 +23,11 @@ typedef struct {
     int end;
 } Queue;
 
+typedef struct {
+    pthread_mutex_t queueLock;
+    pthread_mutex_t generalLock;
+} Locks;
+
 typedef enum {FALSE = 0, TRUE} Boolean;
 
 // job queue - global variable
@@ -33,6 +38,8 @@ int internal_count = 0;
 Boolean shutDown = FALSE;
 // thread array -- global variable
 pthread_t threads[ARR_SIZE];
+// locks -- global variable
+Locks locks;
 
 /*******************************************************************************
 * function name : exitWithError
@@ -60,6 +67,12 @@ void initQueue(int size) {
 Boolean enqueue(char mission) {
     // if the queue is null no access
     if (jobQueue.queue == NULL) return FALSE;
+
+    // lock the queue
+    if((pthread_mutex_lock(&(locks.queueLock)) != 0)) {
+        exitWithError("mutex lock error");
+    }
+
     // if the queue is full -> allocate dynamically more
     if (jobQueue.end == (jobQueue.qSize - 1)) {
         jobQueue.queue = (char *) realloc(jobQueue.queue, 2 * jobQueue.qSize);
@@ -71,12 +84,26 @@ Boolean enqueue(char mission) {
 
     // enqueue the mission
     jobQueue.queue[jobQueue.end++] = mission;
+
+    // unlock the queue
+    if((pthread_mutex_unlock(&(locks.queueLock)) != 0)) {
+        exitWithError("mutex unlock error");
+    }
+    
     return TRUE;
 }
 
 char dequeue() {
+    char c;
+
     // if the queue is null no access
     if (jobQueue.queue == NULL) return FALSE;
+
+    // lock the queue
+    if((pthread_mutex_lock(&(locks.queueLock)) != 0)) {
+        exitWithError("mutex lock error");
+    }
+
     // if the queue is empty
     if (jobQueue.front == jobQueue.end) {
         jobQueue.front = 0;
@@ -85,15 +112,84 @@ char dequeue() {
     }
 
     // return first in line
-    return jobQueue.queue[jobQueue.front++];
+    c = jobQueue.queue[jobQueue.front++];
+
+    // unlock the queue
+    if((pthread_mutex_unlock(&(locks.queueLock)) != 0)) {
+        exitWithError("mutex unlock error");
+    }
+
+    return c;
 }
 
 void freeQueue() {
     free(jobQueue.queue);
 }
 
-void initThreadPool() {
+void* mainThreadFunction(void * args) {
+    Task task;
+    char c;
 
+    while (TRUE) {
+        // exit the thread while shutdown
+        if (shutDown) break;
+
+        // get the task to execute
+        c = dequeue();
+
+        // assign correct function
+        switch (c) {
+            case 0:
+                continue;
+            case 'a':
+                break;
+            case 'b':
+                break;
+            case 'c':
+                break;
+            case 'd':
+                break;
+            case 'e':
+                break;
+            case 'f':
+                break;
+            default:
+                break;
+        }
+
+        // start working
+        (*(task.function))(NULL);
+    }
+
+    if (shutDown) {
+        // run function that writes to the files --case h
+    }
+
+
+    // return null
+    return ((void *)0);
+}
+
+void initThreadPool(int qSize) {
+    int i;
+    // init the job queue
+    initQueue(qSize);
+
+    // initialize mutex
+    if (pthread_mutex_init(&(locks.queueLock), PTHREAD_MUTEX_ERRORCHECK) != 0) {
+        exitWithError("mutex init error");
+    }
+    // initialize mutex
+    if (pthread_mutex_init(&(locks.generalLock), PTHREAD_MUTEX_ERRORCHECK) != 0) {
+        exitWithError("mutex init error");
+    }
+
+    // create the threads
+    for (i = 0; i < ARR_SIZE; ++i) {
+        if ((pthread_create((threads + i), NULL, mainThreadFunction, (void *)&locks)) != 0) {
+            exitWithError("pthread create error");
+        }
+    }
 }
 
 int main(int argc, char **argv) {
